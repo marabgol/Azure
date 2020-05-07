bde721e40c80dec9c17e2f7f555a932fb48fbfaf
Majid Arabgol <majid@Majids-MBP.attlocal.net>
2020-05-07 13:34:36 -0400

    add linux extenstion gor gpu

---
+++ gpu_linux_driver_extenstion.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+export myResourceGroup = Majid_HPC_testbed-zs4jjz3kv5cz3g6xkj5pyekcou
+export myVM = execute-z7z73kcew5hyt 
+az vm extension set \
+  --resource-group myResourceGroup \
+  --vm-name myVM \
+  --name NvidiaGpuDriverLinux \
+  --publisher Microsoft.HpcCompute \
+  --version 1.2 \
+  --settings '{ \
+    "updateOS": true, \
+    "driverVersion": "9.1.85", \
+  }'
